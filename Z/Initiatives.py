20a512b98895c14019da80045103dd2fd514329c
kireitensh <karina.gornicka@wp.pl>
2023-02-20 18:05:35 +0100

    refactor

--- Z/Initiatives.py
+++ Z/Initiatives.py
@@ -3,9 +3,10 @@ from Z.Meeting import Meeting
 
 class Initiatives(Meeting):
     def __init__(self, name, description, day, hour, location):
-        super().__init__(name, day, location)
+        super().__init__(name, location)
         self.description = description  # opis inicjatywy
         self.hour = hour  # godzina inicjatywy
+        self.day = day
 
     def get_full_description(self):
         return f"{self.name} {self.description} held on {self.day} at {self.hour}"
