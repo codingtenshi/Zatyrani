bff966a408870f97a1695355532df3953233dec7
kireitensh <karina.gornicka@wp.pl>
2023-02-20 16:15:54 +0100

    refactor

--- Z/Competition.py
+++ Z/Competition.py
@@ -4,15 +4,6 @@ class Competition:
         self.location = location
         self.date = date
 
-    def get_name(self):
-        return self.name
-
-    def get_location(self):
-        return self.location
-
-    def get_date(self):
-        return self.date
-
     def get_full_description(self):
         return f"{self.name} { self.location} held on {self.date}"
 
