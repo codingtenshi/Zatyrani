a9047775019d6d816862c97845918500201f2348
kireitensh <karina.gornicka@wp.pl>
2023-03-14 12:16:23 +0100

    refactor

--- testy_instrukcje_warunkowe/test_is_palindrome.py
+++ testy_instrukcje_warunkowe/test_is_palindrome.py
@@ -3,7 +3,11 @@ from instrukcje_warunkowe.is_string_palindrome import is_palindrome
 
 def test_is_palindrome():
     a = "racecar"
-    expected = "Palindrome"
+    b = "peace"
+    expected1 = "Palindrome"
+    expected2 = "Not palindrome"
 
-    result = is_palindrome(a)
-    assert result == expected
+    result1 = is_palindrome(a)
+    result2 = is_palindrome(b)
+    assert result1 == expected1
+    assert result2 == expected2
