b1d520be7025ad57f8d1591ce762c6cfeb013e01
kireitensh <karina.gornicka@wp.pl>
2023-03-14 12:16:04 +0100

    refactor

--- testy_instrukcje_warunkowe/test_get_positive_number.py
+++ testy_instrukcje_warunkowe/test_get_positive_number.py
@@ -3,11 +3,18 @@ from instrukcje_warunkowe.is_number_positive_negative_or_zero import get_positiv
 
 def test_get_positive_number():
     a = 5
-    expected = True
+    b = 0
+    c = -5
+    expected1 = "Positive"
+    expected2 = "Zero"
+    expected3 = "Negative"
 
-    result = get_positive_number(a)
-    assert result == expected
-
+    result1 = get_positive_number(a)
+    result2 = get_positive_number(b)
+    result3 = get_positive_number(c)
+    assert result1 == expected1
+    assert result2 == expected2
+    assert result3 == expected3
 
 
 
