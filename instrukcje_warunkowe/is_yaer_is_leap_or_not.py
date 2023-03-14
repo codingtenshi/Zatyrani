9df152289b690a7a65f5e8743aa53a403e4c651a
kireitensh <karina.gornicka@wp.pl>
2023-03-14 12:22:41 +0100

    refactor

--- instrukcje_warunkowe/is_yaer_is_leap_or_not.py
+++ instrukcje_warunkowe/is_yaer_is_leap_or_not.py
@@ -1,6 +1,8 @@
 def get_leap_year(year):
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    if year % 4 == 0 and year % 100 != 0:
         return "Leap year"
     else:
         return "Not leap year"
+
+
 
