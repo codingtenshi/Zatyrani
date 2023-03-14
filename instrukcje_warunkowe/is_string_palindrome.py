b54e3637a46862bea6ea94c202a7a8ca543cc8a4
kireitensh <karina.gornicka@wp.pl>
2023-03-14 11:22:42 +0100

    add conditional statement(is string palindrome)

---
+++ instrukcje_warunkowe/is_string_palindrome.py
@@ -0,0 +1,12 @@
+#  write a program that checks whether a string is a palindrome or not
+#  input; racecar
+#  expected output: Palindrome
+
+def is_palindrome(a):
+    if a == a[:: -1]:
+        return "Palindrome"
+    else:
+        return False
+
+
+
